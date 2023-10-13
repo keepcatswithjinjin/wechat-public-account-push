57905a989856c0b71654c474dd3e35efb892f7d9
keepcatswithjinjin <92412259+keepcatswithjinjin@users.noreply.github.com>
2023-10-13 20:30:00 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,10 +8,10 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wx55959d1b6b341a77',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: '23d072d7d1eef1cbc0f1f2f1feef0237',
 
   PROVINCE: '广东',
   CITY: '惠州',
@@ -51,13 +51,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'taxUpx-7O7lbye4UIRDZaNZzlhMnWpaz_e1Nu8MxDw4',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oIHvR6piHGpgvApoa0Wh6EmI-4S0',
     }
   ],
 
