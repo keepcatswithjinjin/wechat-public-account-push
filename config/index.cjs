6175cd5ac37db0a4fa77bd188fb6f4098547d8af
keepcatswithjinjin <92412259+keepcatswithjinjin@users.noreply.github.com>
2023-10-14 23:05:00 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oIHvR6piHGpgvApoa0Wh6EmI-4S0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '5aOSSYNthVyQ4qJjmM4Ce8w7xtvijxOaBIe3qKIaQnE',
+      useTemplateId: 's_OFoK7_l4eo_hOpx_uzo_b0QFjZzBTjsPDNg1UOOU4',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
