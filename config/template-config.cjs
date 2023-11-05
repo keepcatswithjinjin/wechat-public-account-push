8de405d00afc1f6ef43f5baf9a835f6f15ddce63
keepcatswithjinjin <92412259+keepcatswithjinjin@users.noreply.github.com>
2023-11-05 13:33:06 +0800

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -21,11 +21,8 @@ const TEMPLATE_CONFIG = [
       风级: {{wind_scale.DATA}}
       
       {{comprehensive_horoscope.DATA}}
-      {{healthy_horoscope_0.DATA}}
+      {{healthy_horoscope.DATA}}
       
-      {{healthy_horoscope_1.DATA}}
-      
-      {{healthy_horoscope_2.DATA}}
       ---
       
       今天是我们相识的第{{love_day.DATA}}天
