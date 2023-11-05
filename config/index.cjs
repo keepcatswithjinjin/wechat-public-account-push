cd84bdae08598eec1918252c4a9e50b86d21bf0d
keepcatswithjinjin <92412259+keepcatswithjinjin@users.noreply.github.com>
2023-11-05 14:00:06 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oIHvR6piHGpgvApoa0Wh6EmI-4S0',
+      id: 'oIHvR6mQmiQQk7VnY3ZBk8nm6Q4g',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'ppCN1hA6uq0fvJO-P38XkCaWyazCTr4KYB4v6SaNBMk',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
